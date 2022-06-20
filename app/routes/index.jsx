7e332e902580d79a17a887475d017db97ad4bc2b
Yehudah <you@example.com>
2022-06-20 16:43:59 +0300

    bla

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,101 +1,149 @@
 export default function Index() {
   return (
     <>
-      <div className="bg-gradient-to-r from-white to-sky-600 h-30 p-10 text-center">
-        <h1>Welcome to PE safe</h1>
-        <div className="text-orange-500">navbar </div>
-      </div>
-
-      <div className="text-blue-200 h-100 p-20 text-center">
-        <video
-          autoPlay
-          muted
-          loop
-          src="/public/img/background.mp4"
-          id="myvideo"
-          type="video/mp4"
-        ></video>
-      </div>
-
-      <div className="md:columns-2">
-        <div className="w-full bg-white p-10">
-          <p>
-            PE safe is an integrated IT solution provider and consultancy
-            harnessing cyber security for business continuity and digital asset
-            protection
-          </p>
-          <button>Consultancy</button>
-          <button>Infrastructure</button>
-          <button>DevOps</button>
-          <button>AI/ML</button>
-        </div>
-        <div id="x" className="w-full bg-orange-200">
-          <img src="img/cloud.png" alt="lock"/>
-        </div>
-      </div>
-
-      <div>
-      <video autoplay muted loop id="myVideo">
-  <source src="/public/img/background.mp4" type="video/mp4">
-</video>
-</div>
-
-      <div className="bg-cyan-900 text-white p-10">
-        <h2 className="text-center">Securing IT and Knowledge assets</h2>
-        <div className="columns-3 text-green-500">
-          <div className="p-4 ">
-            <p>Peace of mind</p>
-          </div>
-          <div>
-            <p>Reduced downtime</p>
-          </div>
-          <div>
-            <p>Faster deployment of software</p>
+      <head>
+        <title>PE safe</title>
+      </head>
+      <body>
+        {/* <title>PE safe</title> */}
+        <div className="flex bg-gradient-to-r from-white to-sky-600 p-10 justify-center items-center h-24">
+          <img className="h-24" src="img/logo.png" alt="logo" />
+          <div className="w-36"></div>
+          <div className="w-36"></div>
+          <div className="mx-20 flex space-x-14">
+            <a
+              className="mt-4 hover:bg-emerald-300 rounded px-2 py-1 transition-all"
+              href="/"
+            >
+              home
+            </a>
+            <a
+              className="mt-4 hover:bg-emerald-300 rounded px-2 py-1 transition-all"
+              href="/"
+            >
+              our services
+            </a>
+            <a
+              className="mt-4 hover:bg-emerald-300 rounded px-2 py-1 transition-all"
+              href="/"
+            >
+              about us
+            </a>
+            <a
+              className="mt-4 hover:bg-emerald-300 rounded px-2 py-1 transition-all"
+              href="/"
+            >
+              our team
+            </a>
+            <a
+              className="mt-4 hover:bg-emerald-300 rounded px-2 py-1 transition-all"
+              href="/"
+            >
+              contact us
+            </a>
           </div>
         </div>
-        <ol className="text-center">
-          <li>Customers trust your brand with their data</li>
-          <li>
-            Secure IT services and ongoing maintenance reduces the likelihood of
-            an IT service or digital asset attack
-          </li>
-          <li>
-            Peace of mind – you are in good hands with PESafe so you can focus
-            on your business
-          </li>
-        </ol>
-      </div>
 
-      <div className="columns-2">
-        <div>
-          <h2>A CLEAR HOLISTIC BUSINESS-LED APPROACH</h2>
-          <p>
-            Where IT and knowledge assets are protected in a controlled way, to
-            support the way your business functions and uses IT.
-          </p>
+        <div className="text-white flex w-max bg-slate-500">
+          <div id="x1">
+            <h1 className="absolute text-9xl m-28 mt-60 font-mono">
+              Secure IT
+            </h1>
+            <h3 className="absolute text-4xl mt-96 ml-28 text-emerald-500 italic font-bold">
+              USE US TO SECURE ALL OF YOUR ONLINE ASSETS
+            </h3>
+            <video
+              className="w-screen bg-no-repeat bg-cover"
+              autoPlay
+              muted
+              loop
+              src="img/background.mp4"
+              alt="vid"
+            />
+          </div>
         </div>
-        <div>
-          <img src="/app/img/chess.jpeg" alt="chess" />
+        <div className="md:columns-2">
+          <div className="w-full bg-white p-10 h-">
+            <p>
+              PE safe is an integrated IT solution provider and consultancy
+              harnessing cyber security for business continuity and digital
+              asset protection
+            </p>
+            <button className="border-2 rounded-md w-36 m-7 px-4 py-2 text-green-500 bg-cyan-900 font-mono">
+              Consultancy
+            </button>
+            <button className="border-2 rounded-md w-36 m-7 px-2 py-2 text-green-500 bg-cyan-900 font-mono">
+              Infrastructure
+            </button>
+            <button className="border-2 rounded-md w-36 m-7 px-4 py-2 text-green-500 bg-cyan-900 font-mono">
+              DevOps
+            </button>
+            <button className="border-2 rounded-md w-36 m-7 px-4 py-2 text-green-500 bg-cyan-900 font-mono">
+              AI/ML
+            </button>
+          </div>
+          <div id="x" className="hidden md:block">
+            <img src="img/cloud.png" alt="lock" />
+          </div>
         </div>
-      </div>
 
-      <div className="bg-sky-600">
-        <h2>Secure IT Across the Enterprise</h2>
-        <p>
-          <em>We protect your assets in 4 areas:</em>
-        </p>
+        <div className="bg-cyan-900 text-white p-10">
+          <h2 className="text-center">Securing IT and Knowledge assets</h2>
+          <div className="columns-3 text-green-500">
+            <div className="p-4 ">
+              <p>Peace of mind</p>
+            </div>
+            <div>
+              <p>Reduced downtime</p>
+            </div>
+            <div>
+              <p>Faster deployment of software</p>
+            </div>
+          </div>
+          <ol className="text-center">
+            <li>Customers trust your brand with their data</li>
+            <li>
+              Secure IT services and ongoing maintenance reduces the likelihood
+              of an IT service or digital asset attack
+            </li>
+            <li>
+              Peace of mind – you are in good hands with PESafe so you can focus
+              on your business
+            </li>
+          </ol>
+        </div>
+
         <div className="columns-2">
           <div>
-            <p>Hardware</p>
-            <p>Security processes and auditing</p>
+            <h2>A CLEAR HOLISTIC BUSINESS-LED APPROACH</h2>
+            <p>
+              Where IT and knowledge assets are protected in a controlled way,
+              to support the way your business functions and uses IT.
+            </p>
           </div>
           <div>
-            <p>software</p>
-            <p>security training</p>
+            <img src="img/chess.jpeg" alt="chess" />
           </div>
-          <div></div>
         </div>
-      </div>
+
+        <div className="bg-sky-600">
+          <h2>Secure IT Across the Enterprise</h2>
+          <p>
+            <em>We protect your assets in 4 areas:</em>
+          </p>
+          <div className="columns-2">
+            <div>
+              <p>Hardware</p>
+              <p>Security processes and auditing</p>
+            </div>
+            <div>
+              <p>software</p>
+              <p>security training</p>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </body>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {backgroundImage: {
+      'bg-pattern': "url('/img/background.mp4')",
+      'footer-texture': "url('/img/footer-texture.png')",
+    }},
   },
   plugins: [],
 };
